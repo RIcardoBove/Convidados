f993ffa36a35339308172d844869569a6cc298e7
Ricardo Bove <ricardobove57@gmail.com>
2022-12-18 19:06:35 -0300

    Criando insert e update

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -1,21 +1,61 @@
 package com.example.convidados.repository
 
-class GuestRepository private constructor(){
+import android.content.ContentValues
+import android.content.Context
+import com.example.convidados.constants.DataBaseConstants
+import com.example.convidados.model.GuestModel
+
+class GuestRepository private constructor(context: Context) {
+
+    private val guestDataBase = GuestDataBase(context)
 
     // Singleton
     companion object {
         private lateinit var repository: GuestRepository
 
-        fun getIntance(): GuestRepository {
+        fun getIntance(context: Context): GuestRepository {
             if (!Companion::repository.isInitialized) {
-                repository = GuestRepository()
+                repository = GuestRepository(context)
             }
             return repository
         }
     }
 
-    fun save() {
+    fun insert(guest: GuestModel): Boolean {
 
+        return try {
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, values)
+            true
+
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun update(guest: GuestModel): Boolean {
+
+        return try {
+
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 
 }
--- app/src/main/java/com/example/convidados/viewmodel/GuestFormViewmModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/GuestFormViewmModel.kt
@@ -1,9 +1,16 @@
 package com.example.convidados.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.example.convidados.model.GuestModel
 import com.example.convidados.repository.GuestRepository
 
-class GuestFormViewmModel : ViewModel() {
+class GuestFormViewmModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository = GuestRepository.getIntance()
+    private val repository = GuestRepository.getIntance(application)
+
+    fun insert(guest: GuestModel ) {
+           repository.insert(guest)
+    }
 }
